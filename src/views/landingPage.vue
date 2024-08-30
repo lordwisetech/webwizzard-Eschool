d469a640f3ef8fb0c4eca53e2a108a5e77d58ebb
mayowa <matthewoluwatotin2020@gmail.com>
2024-08-31 11:29:28 +1245

    remove some componet

--- src/views/landingPage.vue
+++ src/views/landingPage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import {ref} from 'vue'
-import landnav from '../components/landnav.vue'
+
 import firstsection from '@/components/firstSection.vue'
 import course from '../components/course.vue'
 import testimony from '@/components/testimony.vue'
-import step from '@/components/sideNav.vue'
+
 import foooter from '@/components/footer.vue'
 
 
@@ -12,11 +12,10 @@ import foooter from '@/components/footer.vue'
 
 <template>
   
-   <landnav></landnav> 
+   
    <firstsection></firstsection>
-   <course></course>
    <testimony></testimony>
-   <step></step>
+   
    <foooter> </foooter>
  
 
